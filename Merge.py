f54782cd1ac816f6b00e006377e93a22cb3e4583
Vortes <zeyu.weng@gmail.com>
2019-12-14 13:53:43 -0500

    merge.py

--- Merge.py
+++ Merge.py
@@ -5,17 +5,42 @@ def find_factors(num):
             factors.append(i)
     return factors
 
+
+def remove_duplicates(input):
+    final_list = []
+    for i in input:
+        if i not in final_list:
+            final_list.append(i)
+    return final_list
+
+
 def merge_the_tools(string, k):
     # bad naming due to consistency for hackerranks problem
     n = len(string)
     k = max(find_factors(n))
     equal_parts = int(n/k)
+    dummy_ti = []
     ti = []
     ui = []
 
     for t in range(0, n, equal_parts):
-        ti.append(string[t:t + equal_parts])
+        dummy_ti.append(string[t:t + equal_parts])
     
+    for t in range(len(dummy_ti)):
+        ti.append(list(dummy_ti[t]))
+
+    for i in range(len(ti)):
+        ui.append(remove_duplicates(ti[i]))
+    
+    for i in range(len(ui)):
+        if i == 0:
+            pass
+        else:
+            print()
+        for j in range(len(ui[i])):
+            print(ui[i][j], end='')
+    
+        
 
 if __name__ == '__main__':
     string, k = input(), int(input())
