cf561b227dff75a196fb2aed8c6e04f49e99d4b5
Vortes <zeyu.weng@gmail.com>
2019-12-14 19:59:55 -0500

    finished merge

--- Merge.py
+++ Merge.py
@@ -1,10 +1,3 @@
-def find_factors(num):
-    factors = []
-    for i in range(1, num):
-        if num % i == 0:
-            factors.append(i)
-    return factors
-
 
 def remove_duplicates(input):
     final_list = []
@@ -17,21 +10,19 @@ def remove_duplicates(input):
 def merge_the_tools(string, k):
     # bad naming due to consistency for hackerranks problem
     n = len(string)
-    k = max(find_factors(n))
-    equal_parts = int(n/k)
     dummy_ti = []
     ti = []
     ui = []
 
-    for t in range(0, n, equal_parts):
-        dummy_ti.append(string[t:t + equal_parts])
+    for t in range(0, n, k):
+        dummy_ti.append(string[t:t + k])
     
     for t in range(len(dummy_ti)):
         ti.append(list(dummy_ti[t]))
 
     for i in range(len(ti)):
         ui.append(remove_duplicates(ti[i]))
-    
+
     for i in range(len(ui)):
         if i == 0:
             pass
